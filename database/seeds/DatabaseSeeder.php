c3d7dc7b00154ab57d323d90ae5fec0ae464f84d
pjsinco <psinco@fas.harvard.edu>
2015-05-26 09:43:45 -0500

    bring in faker and use it to seed a few Article rows

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,28 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
-		// $this->call('UserTableSeeder');
+        $this->call('ArticlesTableSeeder');
 	}
+
+}
+
+class ArticlesTableSeeder extends Seeder {
+
+    public function run() 
+    {
+        $faker = Faker\Factory::create();
+        
+        $feature = App\Article::create([
+            'wp_id' => '11111',
+            'type' => 'feature',
+            'bottom_rule' => '1',
+            'kicker' => implode(' ', $faker->words(3)),
+            'title' => $faker->sentence(10),
+            'excerpt' => implode('', $faker->sentences(2)),
+            'link' => $faker->url,
+            'img_alt' => implode(' ', $faker->words(4)),
+            'img_src' => $faker->imageUrl(480, 320, 'cats'),
+        ]);
+    }
 
 }
